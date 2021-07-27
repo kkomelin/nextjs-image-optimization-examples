592352f09817fc409e7e0d19124cc839b28f0f3e
Konstantin Komelin <konstantin.komelin@gmail.com>
2021-07-27 08:12:44 +0300

    Implemented the first example.

---
+++ pages/unoptimized.js
@@ -0,0 +1,41 @@
+export default function Unoptimized() {
+  return (
+    <div className="container">
+      {/* Original resolution: 4032x3024 (4x3) */}
+      {/* Original size: ~1.9mb */}
+      <img 
+        src="/sunset1.jpg" 
+        alt="Sunset" 
+        width={600} 
+        height={450} 
+      />
+      
+      {/* Original resolution: 4032x3024 (4x3) */}
+      {/* Original size: ~2.3mb */}
+      <img 
+        src="/mountains1.jpg" 
+        alt="Mountains" 
+        width={600} 
+        height={450} 
+      />
+
+      {/* Original resolution: 4032x3024 (4x3) */}
+      {/* Original size: ~1.9mb */}
+      <img 
+        src="/sunset2.jpg" 
+        alt="Sunset" 
+        width={600} 
+        height={450} 
+      />
+      
+      {/* Original resolution: 4032x3024 (4x3) */}
+      {/* Original size: ~2.3mb */}
+      <img 
+        src="/mountains2.jpg" 
+        alt="Mountains" 
+        width={600} 
+        height={450} 
+      />
+    </div>
+  )
+}
